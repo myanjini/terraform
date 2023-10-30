5f26a890b7a9eec45f655fbbe51672ef0156c0a9
myanjini <myanjini@gmail.com>
2023-10-30 14:41:07 +0900

    Made some changes to webserver-cluster

--- services/webserver-cluster/main.tf
+++ services/webserver-cluster/main.tf
@@ -14,14 +14,12 @@ resource "aws_launch_configuration" "example" {
   instance_type   = var.instance_type
   security_groups = [aws_security_group.instance.id]
 
-  # Render the User Data script as a template
   user_data = templatefile("${path.module}/user-data.sh", {
     server_port = var.server_port
     db_address  = data.terraform_remote_state.db.outputs.address
     db_port     = data.terraform_remote_state.db.outputs.port
   })
 
-  # Required when using a launch configuration with an auto scaling group.
   lifecycle {
     create_before_destroy = true
   }
@@ -102,27 +100,10 @@ resource "aws_lb_listener" "http" {
   }
 }
 
-# resource "aws_security_group" "alb" {
-#   name = "${var.cluster_name}-alb"
-
-#   ingress {
-#     from_port   = local.http_port
-#     to_port     = local.http_port
-#     protocol    = local.tcp_protocol
-#     cidr_blocks = local.all_ips
-#   }
-
-#   egress {
-#     from_port   = local.any_port
-#     to_port     = local.any_port
-#     protocol    = local.any_protocol
-#     cidr_blocks = local.all_ips
-#   }
-# }
-
 resource "aws_security_group" "alb" {
   name = "${var.cluster_name}-alb"
 }
+
 resource "aws_security_group_rule" "allow_http_inbound" {
   type              = "ingress"
   security_group_id = aws_security_group.alb.id
@@ -131,6 +112,7 @@ resource "aws_security_group_rule" "allow_http_inbound" {
   protocol          = local.tcp_protocol
   cidr_blocks       = local.all_ips
 }
+
 resource "aws_security_group_rule" "allow_all_outbound" {
   type              = "egress"
   security_group_id = aws_security_group.alb.id
@@ -162,5 +144,3 @@ resource "aws_lb_target_group" "asg" {
     unhealthy_threshold = 2
   }
 }
-
-
